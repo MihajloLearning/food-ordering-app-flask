38d6616d19d50a7c1ccfa5813fac76de3695b027
Mihajlo Dimeski <mihajlo.dimeski@iwconnect.com>
2025-11-18 12:06:25 +0100

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,11 @@ def create_app():
 
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # Add robust connection pooling
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        "pool_pre_ping": True,
+        "pool_recycle": 300,
+    }
 
     db.init_app(app)
 
